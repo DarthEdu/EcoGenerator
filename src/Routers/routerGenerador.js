4154a6b3c5b9fd84003023882cba07e2daa57648
JoseBernal06 <150805766+JoseBernal06@users.noreply.github.com>
2024-11-30 21:54:10 -0500

    imagenes generaodores

--- src/Routers/routerGenerador.js
+++ src/Routers/routerGenerador.js
@@ -1,21 +1,20 @@
 
 import {Router} from 'express'
+import { VerifyToken } from '../Middleware/auth.js'
 import { createGeneradorController, deleteGeneradorController, getAllGeneradorControllerByID, getAllGeneradoresController, updateGeneradorController } from '../Controllers/controladorGenerador.js'
-
 
 const router=Router()
 
-// ver generadores
+// rutas publicas
 router.get('/generador', getAllGeneradoresController)
-router.get('/generador/:id', getAllGeneradorControllerByID)
+router.get('/generador/q=:id', getAllGeneradorControllerByID)
 
-//eliminar generador
-router.delete('/generador/:id', deleteGeneradorController)
-
-// actualizar generador
-router.put('/generador/:id', updateGeneradorController)
-
-//crear generador
-router.post('/generador', createGeneradorController)
+// rutas privadas
+router.delete('/generador/q=:id',VerifyToken,deleteGeneradorController)
+router.put('/generador/q=:id',VerifyToken,updateGeneradorController)
+router.post('/generador',VerifyToken,createGeneradorController)
 
 export default router
+
+
+
